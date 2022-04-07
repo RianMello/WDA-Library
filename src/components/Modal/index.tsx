dcace73b694b1c95ead207f1a9c322aac93e4e4d
RianMello <rianfmello05@gmail.com>
2022-04-07 16:42:11 -0400

    feat: implemented user listing

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,12 @@
+import { Modal } from "@mui/material";
+import { ReactNode } from "react";
+
+interface ModalProps {
+  children: ReactNode;
+}
+
+const ModalComponent = ({ children }: ModalProps) => {
+  return <></>;
+};
+
+export default Modal;
